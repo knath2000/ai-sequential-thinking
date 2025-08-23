f36fab7947a64593b144f0e7eddeb8e395820549
Kalyan Nath <kalyannath@Kalyans-MacBook-Pro.local>
2025-08-23 12:11:47 -0700

    another vercel output dir fix

--- admin-dashboard/.svelte-kit/output/server/chunks/internal.js
+++ admin-dashboard/.svelte-kit/output/server/chunks/internal.js
@@ -2076,7 +2076,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "k3cl31"
+  version_hash: "fa74or"
 };
 async function get_hooks() {
   let handle;
